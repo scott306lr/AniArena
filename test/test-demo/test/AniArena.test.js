9a537e15a48d74b342826a48fa336e1de74247ac
scott306lr <scott306lr@gmail.com>
2022-03-17 15:39:47 +0800

    added skill json

--- test/test-demo/test/AniArena.test.js
+++ test/test-demo/test/AniArena.test.js
@@ -33,7 +33,35 @@ class Character{
     const data = {
       "Name": "Star",
       "Rarity": "SS",
-      "Skills": [1, 3, 5, 11, 21]
+      "Skills": [
+        {
+          ID: 3,
+          Name: "test3",
+          Type: "Attack",
+          Description: "$name uses Mega Beam!",
+          ATK_Val: 10,
+          DEF_Val: 0,
+          AP_Cost: 5,
+        },
+        {
+          ID: 25,
+          Name: "test25",
+          Type: "React",
+          Description: "$name Shields!",
+          ATK_Val: 0,
+          DEF_Val: 5,
+          AP_Cost: 0,
+        },
+        {
+          ID: 113,
+          Name: "test113",
+          Type: "React",
+          Description: "$name blocks and counter attacks!",
+          ATK_Val: 3,
+          DEF_Val: 3,
+          AP_Cost: 0,
+        },
+      ]
     }
     return data;
   }

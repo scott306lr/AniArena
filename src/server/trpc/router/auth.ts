bed16307f3cde658671a68b4b74a59de1eb2f396
scott306lr <scott306lr@gmail.com>
2022-08-17 01:14:47 +0800

    better example message

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -5,8 +5,8 @@ export const authRouter = t.router({
   getSession: t.procedure.query(({ ctx }) => {
     return ctx.session;
   }),
-  getSecretMessage: authedProcedure.query(({ ctx }) => {
-    return `You are logged in and can see this secret message! \n${ctx.session}`;
+  getSecretMessage: authedProcedure.query(() => {
+    return `You are logged in and can see this secret message!`;
   }),
   getSecretTest: authedProcedure.input(z.object({myname: z.string()})).query(({ ctx, input }) => {
     console.log("Loggin ctx.session, input, from getSecretTest")

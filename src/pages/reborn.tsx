c54fd9f05ee3027352ab17b97f33ae039dc62dd8
kaijhsu <hikper@gmail.com>
2022-08-25 12:28:30 +0800

    reborn unlocked char selectable, attr adjustable

--- src/pages/reborn.tsx
+++ src/pages/reborn.tsx
@@ -6,151 +6,117 @@ import React, { useState } from 'react'
 import SkillCard from '../components/SkillCard';
 import AttributeAdjustor from '../components/AttributeAdjustor';
 import Navbar from '../components/Navbar';
-import { trpc } from '../utils/trpc';
+import { inferQueryOutput, trpc } from '../utils/trpc';
 
 const Reborn: NextPage = () => {
-    const [expMax, setExpMax] = useState(4);
-    const [expVal, setExpVal] = useState(1);
-    const [hp, setHp] = useState(7);
+    const [selected, setSelected] = useState(0);
 
-    const increaseExp = () => setExpVal((prev) => Math.min(expMax, expVal+1));
-    const decreaseExp = () => setExpVal((prev) => Math.max(0, expVal-1));
-    const addHp = () => setHp(hp + 1);
-    const minusHp = () => setHp(Math.max(0, hp -1));
-
-    const confirmClick = () => {console.log("confirm clicked")};
-    const resetClick = () => {console.log("reset clicked")};
-    // Todo: fetch data here
     const {data: unlockedChars, isLoading: u_isLoading} = trpc.proxy.me.getUnlockedChar.useQuery();
-    const {data: combater, isLoading: c_isLoading} = trpc.proxy.me.getCombater.useQuery();
-    // console.log(charData)
-
-    // const Avatars = [
-    //     {
-    //         name: "Kirito",
-    //         imgsrc: "https://images-ext-1.discordapp.net/external/x08twzHAcDIWcqHEPgEIdfLNoIGST3tHUdti0Ww3b00/https/mudae.net/uploads/1232276/v6uL2AsAjYy0QE0-4OKy~0P98O5C.png"
-    //     },
-    //     {
-    //         name: "Megumin",
-    //         imgsrc: "https://media.discordapp.net/attachments/872026548692209738/872045442450485288/6fm6YnX.png"
-    //     },
-    //     {
-    //         name: "Saber",
-    //         imgsrc: "https://media.discordapp.net/attachments/872026548692209738/872045678669484062/nkmBV7R.png"
-    //     }
-    // ]
-    
-    const Skills = [
-        {
-            name: "Explosion",
-            type: "攻擊",
-            description: "黑より黑く 闇より暗き漆黑に ，無謬の境界に落ちし理，無行の歪みとなりて，現出せよ！Explosion！"
-        },
-        {
-            name: "Explosion",
-            type: "防禦",
-            description: "防禦魔法！"
-        },
-        {
-            name: "Explosion",
-            type: "攻擊",
-            description: "黑より黑く 闇より暗き漆黑に ，無謬の境界に落ちし理，無行の歪みとなりて，現出せよ！Explosion！"
-        }
-    ]
+    // const {data: combater, isLoading: c_isLoading} = trpc.proxy.me.getCombater.useQuery();
 
     return (
         <div>
             <Navbar />
-            <main className="grid">
-                {/* sections */}
-                <div className="flex flex-wrap justify-center m-8 p-4 gap-8">
-                    {/* middle characters section*/}
-                    {/*Todo: change it to filter map  */}
-                    {/*      Add click event */}
-                    <div className="grid place-content-center p-2">
-                        <ul className="md:grid flex flex-wrap bg-white rounded-lg shadow-lg p-2 space-y-2 hover:scale-110 transition-all">
-                            { 
-                                (u_isLoading || unlockedChars == null) ? (
-                                    <div className="flex justify-center">
-                                        <div className="spinner"></div>
-                                    </div>
-                                ) : (
-                                    unlockedChars.map((character, index) => (
-                                        <li key={index} className="md:w-1/2 md:h-1/2">
-                                            <Avatar imgsrc={character?.image} />
-                                        </li>
-                                    ))
-                                )
-                            }
-                        </ul>
-                    </div>
-
-                    {/* Character status section */}
-                    <div className="grid place-content-center">
-                        <div className="grid w-min justify-items-center gap-4">
-                            { (c_isLoading || combater == null) ? (
-                                    <div className="flex justify-center">
-                                        <div className="spinner"></div>
-                                    </div>
-                                ) : (
-                                    <div>
-                                        <RectCard imgsrc={combater.character?.image}/>
-                                        <p className='word-bubble'>
-                                            {combater.character.description}
-                                        </p>
-                                    </div>
-                                )
-                            }
-                                
-                            <div>
-                                <AttributeBar attribute="EXP" max={expMax} val={expVal}/>
-                                <button onClick={increaseExp} className='action-btn'>
-                                    {"addExp"}
-                                </button>
-                                <button onClick={decreaseExp} className='action-btn'>
-                                    {"minusExp"}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Attribute Secion */}
-                    <div className="grid place-content-center">
-                        <div className="grid gap-4">
-                            <AttributeAdjustor name="HP" value={hp} minusClick={minusHp} addClick={addHp}/>
-                            <AttributeAdjustor name="HP" value={hp} minusClick={minusHp} addClick={addHp}/>
-                            <AttributeAdjustor name="HP" value={hp} minusClick={minusHp} addClick={addHp}/>
-                            <button onClick={confirmClick} className='action-btn'>
-                                {"重設"}
-                            </button>
-                            <button onClick={resetClick} className='action-btn'>
-                                {"確認"}
-                            </button>
-                        </div>
-                    </div>
-
-                    {/* SkillCards Section */}
-                    {/*Todo: change it to filter map  */}
-                    <div className="grid lg:w-1/3 place-content-center">
-                        <ul className="grid gap-4">
-                            { u_isLoading ? (
+            <main className="grid gap-4">
+                {/* middle characters section*/}
+                {/*      Add click event */}
+                <div className="grid place-content-center p-2">
+                    <ul className="flex flex-wrap bg-white rounded-lg shadow-lg">
+                        { 
+                            (u_isLoading || unlockedChars == null) ? (
                                 <div className="flex justify-center">
                                     <div className="spinner"></div>
                                 </div>
-                                ) : (
-                                    Skills.map((skill, index) => (
-                                        <li key={index} className="grid w-full">
-                                            <SkillCard name={skill.name} type={skill.type} description={skill.description} imgsrc="https://media.discordapp.net/attachments/872026548692209738/872045442450485288/6fm6YnX.png"/>
-                                        </li>
-                                    ))
-                                )
-                            }
-                        </ul>
-                    </div>
+                            ) : (
+                                unlockedChars.map((character, index) => (
+                                    <li key={index} className="md:w-1/2 md:h-1/2" onClick={() => setSelected(index)}>
+                                        <Avatar imgsrc={character?.image} />
+                                    </li>
+                                ))
+                            )
+                        }
+                    </ul>
                 </div>
+
+                {/* Character status section */}
+                {u_isLoading || unlockedChars == null ? 
+                    <div>loading...</div> 
+                    : 
+                    <CharacterProfile character={unlockedChars[selected]}/>
+                }
+                
+                
             </main>
         </div>
     );
 }
 
 export default Reborn
+
+
+type CharProfile = inferQueryOutput<"me.getUnlockedChar">[number] | undefined;
+type attr = {
+    AP: number,
+    HP: number,
+    APRegen: number,
+}
+
+// Ensured that profile is not undefined before passing in, but checked again for typescript ensurance
+
+const CharacterProfile: React.FC<{character: CharProfile}> = (props) => {
+    const [attr, setAttr] = useState(props.character?.orgAttr as attr);
+    const addHP = () => setAttr({...attr, HP: attr.HP+1});
+    const minusHP = () => setAttr({...attr, HP: attr.HP-1});
+    const addAP = () => setAttr({...attr, AP: attr.AP+1});
+    const minusAP = () => setAttr({...attr, AP: attr.AP-1});
+    const addAPRegen = () => setAttr({...attr, APRegen: attr.APRegen+1});
+    const minusAPRegen = () => setAttr({...attr, APRegen: attr.APRegen-1});
+
+    // console.log(props.character?.orgAttr)
+
+    const resetClick = () => {setAttr(props.character?.orgAttr as attr)};
+    const confirmClick = () => {console.log("confirm clicked")};
+
+    return (
+        <div className="flex flex-wrap w-10/12 sm:w-auto m-4 p-4 gap-4 rounded-md bg-white shadow-lg justify-self-center justify-center">
+            <div className='grid gap-4 justify-items-center'>
+                <RectCard imgsrc={props.character?.image} />
+                <div className='word-bubble'>{props.character?.description}</div>
+            </div>
+            <div className="grid place-content-center">
+                    <div className="grid gap-4">
+                        <AttributeAdjustor name="HP" value={attr.HP} minusClick={minusHP} addClick={addHP}/>
+                        <AttributeAdjustor name="AP" value={attr.AP} minusClick={minusAP} addClick={addAP}/>
+                        <AttributeAdjustor name="APRegen" value={attr.APRegen} minusClick={minusAPRegen} addClick={addAPRegen}/>
+                        <button onClick={resetClick} className='action-btn'>
+                            {"重設"}
+                        </button>
+                        <button onClick={confirmClick} className='action-btn'>
+                            {"確認"}
+                        </button>
+                    </div>
+            </div>
+                
+        </div>
+    );
+}
+
+
+{/* SkillCards Section */}
+{/*Todo: change it to filter map  */}
+{/* <div className="grid lg:w-1/3 place-content-center">
+    <ul className="grid gap-4">
+        { u_isLoading ? (
+            <div className="flex justify-center">
+                <div className="spinner"></div>
+            </div>
+            ) : (
+                Skills.map((skill, index) => (
+                    <li key={index} className="grid w-full">
+                        <SkillCard name={skill.name} type={skill.type} description={skill.description} imgsrc="https://media.discordapp.net/attachments/872026548692209738/872045442450485288/6fm6YnX.png"/>
+                    </li>
+                ))
+            )
+        }
+    </ul>
+</div> */}

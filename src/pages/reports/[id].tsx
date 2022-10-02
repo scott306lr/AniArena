8282d4eee0e74db6c3164442a7d652f8452451f0
scott306lr <scott306lr@gmail.com>
2022-10-02 18:41:35 +0800

    updated overflow on reports page

--- src/pages/reports/[id].tsx
+++ src/pages/reports/[id].tsx
@@ -7,7 +7,7 @@ import Avatar from '../../components/Avatar';
 import { trpc } from '../../utils/trpc';
 import { useRouter } from 'next/router';
 import { CombatLog } from '../../utils/AniClasses/Arena';
-import { GiHearts, GiBroadsword } from 'react-icons/gi';
+import { GiHearts, GiBroadsword, GiQueenCrown, GiDeathSkull, GiTrophy } from 'react-icons/gi';
 
 const Report: NextPage = () => {
   // Todo: fetch data here
@@ -15,25 +15,25 @@ const Report: NextPage = () => {
   const BattleLogID = parseInt(router.query.id as string);
   const { data: BattleLog, isLoading } = trpc.arena.getBattleLog.useQuery({ id: BattleLogID });
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="flex h-screen flex-col">
       <Navbar />
-      <main className="grid">
+      <main className="flex h-auto justify-center overflow-hidden p-4">
         {/* sections */}
         {BattleLog && (
-          <div className="m-2 flex flex-wrap justify-center gap-10 p-2">
+          <div className="m-2 flex h-auto w-full justify-center gap-10 p-2">
             {/* Character A status section */}
-            <section className="relative flex h-fit w-5/12 justify-items-center rounded-lg bg-slate-50 p-4 md:sticky md:top-16 lg:grid lg:w-min lg:gap-4">
+            <section className="flex h-fit w-5/12 justify-items-center rounded-lg bg-slate-50 p-4 md:top-16 lg:grid lg:w-min lg:gap-4">
               <PlayerInfo id={BattleLog?.creatorId} />
             </section>
 
             {/* Character B status section */}
-            <section className="relative flex h-fit w-5/12 justify-items-center rounded-lg bg-slate-50 p-4 md:sticky md:top-16 lg:order-last lg:grid lg:w-min lg:gap-4">
+            <section className="flex h-fit w-5/12 justify-items-center rounded-lg bg-slate-50 p-4 md:top-16 lg:order-last lg:grid lg:w-min lg:gap-4">
               <PlayerInfo id={BattleLog?.opponentId} />
             </section>
 
-            <section className="w-full lg:hidden"></section>
+            {/* <section className="h-auto w-full lg:hidden"></section> */}
 
-            <section className="flex h-screen w-full overflow-y-auto rounded-md bg-slate-50 lg:w-1/2">
+            <section className="flex h-auto w-full justify-center overflow-hidden rounded-md bg-slate-50 lg:w-1/3 ">
               {BattleLog && <BattleContent context={BattleLog.content as unknown as CombatLog} />}
             </section>
           </div>
@@ -70,23 +70,25 @@ const BattleContent: React.FC<{ context: CombatLog }> = (props) => {
   console.log(props.context);
   const creatorID = props.context.combater1.id;
   return (
-    <div className="flex h-fit w-full items-center justify-center">
-      <ul className="my-4 grid h-fit w-full gap-4 p-2 lg:w-11/12">
-        {props.context.logs.map((round, index) => {
-          return (
-            <li
-              key={index}
-              className={`flex w-full ${round.logger?.id === creatorID ? 'justify-start pr-6' : 'justify-end pl-6'}`}
-            >
-              <p className="word-bubble flex items-center justify-center">
-                {round.logger?.id !== creatorID ? <GiBroadsword /> : <GiHearts />}
-                &nbsp;
-                {round.log}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="flex h-auto w-full justify-center">
+      <div className="h-auto w-full overflow-y-auto">
+        <ul className="my-4 grid h-auto w-auto gap-4  p-2 lg:w-11/12">
+          {props.context.logs.map((round, index) => {
+            return (
+              <li
+                key={index}
+                className={`flex w-full ${round.logger?.id === creatorID ? 'justify-start pr-6' : 'justify-end pl-6'}`}
+              >
+                <p className="word-bubble flex items-center justify-center">
+                  {round.logger?.id !== creatorID ? <GiBroadsword /> : <GiHearts />}
+                  &nbsp;
+                  {round.log}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -18,11 +18,11 @@ const Reports: NextPage = () => {
   // Todo: fetch data here
 
   return (
-    <div>
+    <div className="flex h-screen flex-col">
       <Navbar />
-      <main className="grid">
+      <main className="flex h-auto justify-center overflow-hidden p-4">
         {/* sections */}
-        <section className="m-8 flex justify-center gap-8 p-4">
+        <section className="flex h-auto justify-center gap-8 overflow-hidden p-4">
           {/* middle characters section*/}
           {!isLoading && battleLogs && <ReportList title={'近期戰報'} battleLogs={battleLogs} />}
           {!isLoadingP && battleLogsP && <ReportList title={'熱門戰報'} battleLogs={battleLogsP} />}
@@ -35,45 +35,47 @@ const Reports: NextPage = () => {
 type BattleLogType = inferProcedureOutput<AppRouter['arena']['getBattleLogs']>;
 const ReportList: React.FC<{ title: string; battleLogs: BattleLogType }> = (props) => {
   return (
-    <div className="grid place-content-center gap-2 p-2 text-center">
+    <div className="grid h-auto gap-2 p-2 text-center">
       <h1 className="text-3xl"> {props.title} </h1>
-      <ul className="flex h-auto flex-col space-y-2 rounded-lg bg-white p-2 shadow-lg transition-all hover:scale-110">
-        {props.battleLogs.map((log, index) => {
-          const content = (log.content as unknown as CombatLog) || undefined;
-          if (content == null) {
-            return null;
-          }
+      <div className="h-auto overflow-y-auto rounded-md bg-white shadow-lg">
+        <ul className="flex h-auto flex-col space-y-2 p-4">
+          {props.battleLogs.map((log, index) => {
+            const content = (log.content as unknown as CombatLog) || undefined;
+            if (content == null) {
+              return null;
+            }
 
-          return (
-            <Link key={index} href={`/reports/${log.id}`} passHref>
-              <li className="hover-primary flex h-auto w-full justify-center border-2 border-gray-500">
-                <div className="flex flex-col justify-center px-2">
-                  <h3 className="text-center text-lg">{content?.combater1.name}</h3>
-                  <Avatar
-                    imgsrc={content?.combater1.character.image}
-                    org_width={225}
-                    org_height={350}
-                    className="h-24 w-24"
-                  />
-                </div>
-                {/* <div className="mx-3 h-full w-0.5 bg-gray-600" /> */}
-                <div className="mx-3 flex flex-col justify-center">
-                  <h3 className="h-fit text-lg">v.s.</h3>
-                </div>
-                <div className="flex flex-col justify-center px-2">
-                  <h3 className="text-center text-lg">{content?.combater2.name}</h3>
-                  <Avatar
-                    imgsrc={content?.combater2.character.image}
-                    org_width={225}
-                    org_height={350}
-                    className="h-24 w-24"
-                  />
-                </div>
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
+            return (
+              <Link key={index} href={`/reports/${log.id}`} passHref>
+                <li className="hover-primary flex h-auto w-full justify-center border-2 border-gray-500">
+                  <div className="flex flex-col justify-center px-2">
+                    <h3 className="text-center text-lg">{content?.combater1.name}</h3>
+                    <Avatar
+                      imgsrc={content?.combater1.character.image}
+                      org_width={225}
+                      org_height={350}
+                      className="h-24 w-24"
+                    />
+                  </div>
+                  {/* <div className="mx-3 h-full w-0.5 bg-gray-600" /> */}
+                  <div className="mx-3 flex flex-col justify-center">
+                    <h3 className="h-fit text-lg">v.s.</h3>
+                  </div>
+                  <div className="flex flex-col justify-center px-2">
+                    <h3 className="text-center text-lg">{content?.combater2.name}</h3>
+                    <Avatar
+                      imgsrc={content?.combater2.character.image}
+                      org_width={225}
+                      org_height={350}
+                      className="h-24 w-24"
+                    />
+                  </div>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
